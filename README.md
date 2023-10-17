a9d5ccf93aeb2d0a92ce4be91f0de59a5fb01e54
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-17 06:23:28 +0300

    chore:🌻 add read me

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ekim
+
+```text
+    In this project I want to experiment 
+       Rust in web server
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+mod currency;
+use currency::currency as other_currency;
+
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    rocket::build().mount("/", routes![other_currency::index]).launch().await;
 }
+
