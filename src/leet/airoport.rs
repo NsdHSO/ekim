b1a51754392935ed682a6f8e83a117aa487ef280
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-23 12:11:49 +0300

    chore:🌻 remove all side effects

--- src/leet/airoport.rs
+++ src/leet/airoport.rs
@@ -1,27 +1,16 @@
-use crate::leet::destination::Destination;
-
 #[derive(Debug)]
 pub struct Airport {
     default_size: i32,
     is_snow: bool,
     is_closed: bool,
-    destination_take_off: Vec<Destination>,
 }
 
 impl Airport {
-    pub fn landed(&mut self, destination: Destination) {
-        self.destination_take_off.push(destination)
-    }
     pub fn new() -> Self {
         Airport {
             default_size: 2,
             is_closed: false,
             is_snow: true,
-            destination_take_off: vec![],
         }
-    }
-
-    pub fn get_destinations(&self) -> &Vec<Destination> {
-        &self.destination_take_off
     }
 }
--- src/leet/mod.rs
+++ src/leet/mod.rs
@@ -1,4 +1,1 @@
 pub mod airoport;
-pub mod destination;
-pub mod passenger;
-pub mod remove_duplicate;
